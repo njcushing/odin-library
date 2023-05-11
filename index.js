5decfe94c7e4b921d5101b9b34884078585c2fac
njcushing <129661299+njcushing@users.noreply.github.com>
2023-05-11 12:13:02 +0100

    Add basic function to add new book to library array using console

---
+++ index.js
@@ -0,0 +1,13 @@
+const library = [];
+
+function Book(name, author, pages, read) {
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary(name, author, pages, read) {
+    const newBook = new Book(name, author, pages, read);
+    library.push(newBook);
+}
